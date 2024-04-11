75fca65184ebe3f23254c95ac1001221e12ec770
John Previtera <johngprevitera@gmail.com>
2024-04-11 15:43:35 -0600

    no more bugs

--- src/controllers/BugsController.js
+++ src/controllers/BugsController.js
@@ -9,8 +9,56 @@ export class BugsController extends BaseController {
     constructor() {
         super('api/bugs')
         this.router
+            .get('', this.getBugs)
+            .get('/:bugId', this.getBugById)
             .use(Auth0Provider.getAuthorizedUserInfo)
+            .put('/:bugId', this.updateBug)
             .post('', this.createBug)
+            .delete('/:bugId', this.deleteBug)
+    }
+    async deleteBug(request, response, next) {
+        try {
+            const bugId = request.params.bugId
+            const userInfo = request.userInfo
+            const message = await bugsService.deleteBug(bugId, userInfo.id)
+            response.send(message)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async updateBug(request, response, next) {
+        try {
+            const bugId = request.params.bugId
+            const updateData = request.body
+            const userInfo = request.userInfo
+            updateData.creatorId = userInfo.id
+            const bugToUpdate = await bugsService.updateBug(bugId, updateData)
+            response.send(bugToUpdate)
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    async getBugById(request, response, next) {
+        try {
+            const bugId = request.params.bugId
+            const bug = await bugsService.getBugById(bugId)
+            response.send(bug)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+    async getBugs(request, response, next) {
+        try {
+            const bugs = await bugsService.getBugs()
+            response.send(bugs)
+        } catch (error) {
+            next(error)
+        }
     }
 
 
--- src/services/BugService.js
+++ src/services/BugService.js
@@ -1,9 +1,49 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 
 
 
 class BugsService {
+
+
+    async deleteBug(bugId, id) {
+        const bugToDelete = await this.getBugById(bugId)
+        if (bugToDelete.creatorId != id) throw new Forbidden('Naughty deleting other peoples bugs')
+        await bugToDelete.deleteOne()
+        return bugToDelete
+    }
+
+
+    async updateBug(bugId, updateData) {
+        const bugToUpdate = await this.getBugById(bugId)
+
+
+        if (bugToUpdate.creatorId != updateData.creatorId) throw new Forbidden('Naughty changing other peoples bugs')
+
+
+
+        bugToUpdate.title = updateData.title ?? bugToUpdate.title
+        bugToUpdate.description = updateData.description ?? bugToUpdate.description
+        bugToUpdate.save()
+        return bugToUpdate
+    }
+
+
+    async getBugById(bugId) {
+        const bug = (await dbContext.Bug.findById(bugId)).populate('creator')
+        if (!bugId) throw new Error('That, sir, is not a bug local to this ecosystem.')
+
+        return bug
+    }
+
+
+    async getBugs() {
+        const bugs = await dbContext.Bug.find()
+        return bugs
+    }
+
+
     async createBug(bugData, userInfo) {
         const bug = (await dbContext.Bug.create(bugData)).populate('creator')
         return bug
